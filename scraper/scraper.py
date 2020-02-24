6ce02678d3caffc9176f8a4ecce9a4e6664cb4cd
RasikKane <rasik.kane@gmail.com>
2020-02-24 19:39:51 +0000

    Basic scraper working Database db_24_3_1933.sql [value updated] attached

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,17 +1,27 @@
 import pymysql
 import cryptography
 # Open database connection
-db = pymysql.connect("localhost","root","localhost","dublinbike" )
+dublinBike_connect = pymysql.connect(host="localhost", user="root", password="localhost", database="db_24_2_1933", port=3306,\
+                                      cursorclass=pymysql.cursors.DictCursor)
 
-# prepare a cursor object using cursor() method
-cursor = db.cursor()
 
-# execute SQL query using execute() method.
-cursor.execute("SELECT VERSION()")
+with dublinBike_connect.cursor() as cursor:
+    # Create a new record
+    sql = "INSERT INTO `dublin_bike_dynamicdata` (`number`, `status`) VALUES ('7','open')"
+    cursor.execute(sql)
 
-# Fetch a single row using fetchone() method.
-data = cursor.fetchone()
-print ("Database version : %s " % data)
+# connection is not autocommit by default. So you must commit to save
+# your changes.
+dublinBike_connect.commit()
+
+with dublinBike_connect.cursor() as cursor:
+    # execute SQL query using execute() method.
+    sql = "SELECT `available_bike_stands`, `last_update` FROM `dublin_bike_dynamicdata` WHERE `number`=%s"
+    cursor.execute(sql,('3'))
+
+    # Fetch a single row using fetchone() method.
+    data = cursor.fetchone()
+    print ("Database version : %s " % data)
 
 # disconnect from server
-db.close()
+dublinBike_connect.close()
