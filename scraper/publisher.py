09fed7d9a57608400fd524e6335eaacc40346eb6
RasikKane <rasik.kane@gmail.com>
2020-02-25 18:56:37 +0000

    Dublin bike scrpaer working
    Weather remaining
    
    Challenge: Auto increment for id_entry in dynamic data table preserves 
    memory 
    
    Check latest sql  db_25_2_1855.sql

---
+++ scraper/publisher.py
@@ -0,0 +1,40 @@
+import pymysql
+import cryptography
+
+DB = {"S" : "dublin_bike_staticdata", "D" : "dublin_bike_dynamicdata"}
+arg = {"S" : " VALUES (%s, %s, %s, %s, %s, %s,%s, %s)", "D" : " VALUES (%s, %s, %s, %s, %s, %s)"}
+
+def connectDB():
+
+    return pymysql.connect(host="localhost", user="root", password="localhost", database="db_24_2_1933", port=3306,\
+                                      cursorclass=pymysql.cursors.DictCursor)
+
+def publish(atr, val, table):
+    print('hi',val)
+
+    global DB, arg
+
+    dublinBike_connect = connectDB()
+
+    with dublinBike_connect.cursor() as cursor:
+
+        sql = "INSERT INTO " + DB[table] + atr + arg[table]
+        print(sql)
+        cursor.executemany(sql,val)
+
+    dublinBike_connect.commit()
+
+    dublinBike_connect.close()
+
+
+def Check_StaticEntry(number, table):
+
+    global DB
+
+    dublinBike_connect = connectDB()
+
+    with dublinBike_connect.cursor() as cursor:
+        sql = "SELECT number from " + DB[table] + " where number = %s"
+        cursor.execute(sql,number)
+        dublinBike_connect.close()
+        return cursor.rowcount == 1
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,27 +1,86 @@
-import pymysql
-import cryptography
-# Open database connection
-dublinBike_connect = pymysql.connect(host="localhost", user="root", password="localhost", database="db_24_2_1933", port=3306,\
-                                      cursorclass=pymysql.cursors.DictCursor)
+import json
+import requests
+from publisher import *
+from datetime import datetime
+
+staticAttribute = ['number', 'contract_name', 'name', 'address', 'position_lat', 'position_lng', 'bonus', 'banking']
+dynamicAttribute = ['number', 'status', 'bike_stands', 'available_bike_stands', 'available_bikes']
+tsD = {'Dynamic' : 'last_update'}
+
+# File handling with python https://www.w3schools.com/python/python_file_open.asp
+def api_URL():
+    f = open("apiKey_dBike.txt", "r")
+    contract_name = f.readline().rstrip('\n')
+    api_key = f.readline().rstrip('\n')
+    f.close()
+    api_dBike_URL = "https://api.jcdecaux.com/vls/v1/stations?contract=" + contract_name + "&apiKey=" + api_key
+    return api_dBike_URL
+
+def check_DB_Attribute():
+    pass
+
+def check_instance(dictionary):
+    if isinstance(resp_Fields[key],dict):
+        return True
+    else:
+        return False
+
+# flatten dictionary +sep+https://medium.com/better-programming/how-to-flatten-a-dictionary-with-nested-lists-and-dictionaries-in-python-524fd236365
+def flatten(collection_input,sep="_"):
+    dictionary = {}
+
+    def recurse(coll_in,parent_key=""):
+        if isinstance(coll_in,dict):
+            for key,val in coll_in.items():
+                recurse(val,parent_key+sep+key if parent_key else key)
+        else:
+            dictionary[parent_key] = coll_in
+    recurse(collection_input)
+    return dictionary
 
 
-with dublinBike_connect.cursor() as cursor:
-    # Create a new record
-    sql = "INSERT INTO `dublin_bike_dynamicdata` (`number`, `status`) VALUES ('7','open')"
-    cursor.execute(sql)
+def insert_Data(api_dBike_Resp_json):
+    att_st_final, val_st_final,att_dy_final, val_dy_final = '',[],'',[]
+    DB_dict = {}
 
-# connection is not autocommit by default. So you must commit to save
-# your changes.
-dublinBike_connect.commit()
+    for stations in range(len(api_dBike_Resp_json)):
+        val_st, val_dy =[],[]
 
-with dublinBike_connect.cursor() as cursor:
-    # execute SQL query using execute() method.
-    sql = "SELECT `available_bike_stands`, `last_update` FROM `dublin_bike_dynamicdata` WHERE `number`=%s"
-    cursor.execute(sql,('3'))
+        DB_dict = flatten(api_dBike_Resp_json[stations])
 
-    # Fetch a single row using fetchone() method.
-    data = cursor.fetchone()
-    print ("Database version : %s " % data)
+        for attributeDy in dynamicAttribute:
+            try:
+                val_dy.append(DB_dict[attributeDy])
+            except KeyError:
+                val_dy.append(None)
 
-# disconnect from server
-dublinBike_connect.close()
+        val_dy.append(datetime.fromtimestamp(int(DB_dict[tsD['Dynamic']] / 1000)))
+        val_dy_final.append(val_dy)
+
+
+        if not Check_StaticEntry(DB_dict['number'], "S"):
+            for attributeSt in staticAttribute:
+                try:
+                    val_st.append(DB_dict[attributeSt])
+                except KeyError:
+                    val_st.append(None)
+            val_st_final.append(val_st)
+        else:
+            continue
+
+    att_st_final = "(`" + '`,`'.join(str(sAtt) for sAtt in staticAttribute) +  "`)"
+    att_dy_final = "(`" + '`,`'.join(str(dAtt) for dAtt in dynamicAttribute) +"`,`last_update" + "`)"
+
+    if val_st_final:
+        publish(att_st_final,val_st_final,"S")
+        print('ok')
+    publish(att_dy_final,val_dy_final,"D")
+
+
+def main():
+    api_dBike_Resp = requests.get(api_URL())
+    if api_dBike_Resp.status_code == 200 :
+        insert_Data(api_dBike_Resp.json())
+
+if __name__ == '__main__':
+    main()
